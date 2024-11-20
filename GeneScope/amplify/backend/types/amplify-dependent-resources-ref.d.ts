2dc5555001ee26a0e8e66909b56b444ab87e2c64
jeffano493 <114167911+jeffano493@users.noreply.github.com>
2024-11-20 16:50:04 -0500

    Initialize React application with Vite and AWS Amplify setup

---
+++ GeneScope/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "genescopeamplifybefdd2a5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
