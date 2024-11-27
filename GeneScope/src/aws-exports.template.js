b5c73d232e95e1969ee1f6ab88e7c5b7dcd6f784
jeffano493 <114167911+jeffano493@users.noreply.github.com>
2024-11-27 17:33:38 -0500

    Add AWS exports template and update .gitignore for amplify configuration

---
+++ GeneScope/src/aws-exports.template.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// WARNING: This is a template. Replace placeholders with actual values.
+
+const awsmobile = {
+    "aws_project_region": "YOUR_REGION", // e.g., us-east-1
+    "aws_cognito_identity_pool_id": "YOUR_IDENTITY_POOL_ID",
+    "aws_cognito_region": "YOUR_COGNITO_REGION",
+    "aws_user_pools_id": "YOUR_USER_POOL_ID",
+    "aws_user_pools_web_client_id": "YOUR_WEB_CLIENT_ID",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+export default awsmobile;
