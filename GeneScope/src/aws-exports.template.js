a2721bb57271ca1ef1a28441e73f39e041d6b5ce
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2024-12-02 19:58:52 -0500

    Update aws-exports.template.js

--- GeneScope/src/aws-exports.template.js
+++ GeneScope/src/aws-exports.template.js
@@ -24,7 +24,9 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_user_files_s3_bucket": "genescopefilesec991-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 export default awsmobile;
