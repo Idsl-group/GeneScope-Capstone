174170fa2627dbf3abe4bf5e7d01801b2302f0c6
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2025-03-17 20:41:11 -0400

    navbar updates

--- GeneScope/frontend/components/NavBar.jsx
+++ GeneScope/frontend/components/NavBar.jsx
@@ -1,3 +1,94 @@
+// import React, { useState } from "react";
+// import { useNavigate, useLocation } from "react-router-dom"; 
+// import { signOut } from "aws-amplify/auth";
+// import "./NavBar.css";
+
+// function Navbar({ isLoggedIn, setIsLoggedIn }) {
+//   const [menuOpen, setMenuOpen] = useState(false);
+//   const navigate = useNavigate();
+//   const location = useLocation(); 
+
+//   const handleSignOut = async () => {
+//     try {
+//       await signOut();
+//       setIsLoggedIn(false);
+//       navigate("/");
+//     } catch (err) {
+//       console.error("Error signing out:", err);
+//     }
+//   };
+
+//   const toggleMenu = () => {
+//     setMenuOpen((prevState) => !prevState);
+//   };
+
+//   return (
+//     <div className="floating-navbar">
+//       <span className="nav-logo" onClick={() => navigate("/")}>
+//         Genescope
+//       </span>
+//       <div
+//         className={`hamburger ${menuOpen ? "open" : ""}`}
+//         onClick={toggleMenu}
+//       >
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//       </div>
+
+//       <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+//         <button
+//           className={`nav-button ${
+//             location.pathname === "/" ? "active-tab" : ""
+//           }`}
+//           onClick={() => navigate("/")}
+//         >
+//           Home
+//         </button>
+
+//         {isLoggedIn && (
+//           <>
+//             <button
+//               className={`nav-button ${
+//                 location.pathname === "/fileupload" ? "active-tab" : ""
+//               }`}
+//               onClick={() => navigate("/fileupload")}
+//             >
+//               File Upload
+//             </button>
+
+//             <button
+//               className={`nav-button ${
+//                 location.pathname === "/myfiles" ? "active-tab" : ""
+//               }`}
+//               onClick={() => navigate("/myfiles")}
+//             >
+//               My Files
+//             </button>
+
+            
+//           </>
+//         )}
+
+//         <button
+//           className="nav-button"
+//           onClick={() =>
+//             isLoggedIn ? handleSignOut() : navigate("/authentication")
+//           }
+//         >
+//           {isLoggedIn ? "Log Out" : "Log In"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+
+
+
+
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom"; 
 import { signOut } from "aws-amplify/auth";
@@ -23,7 +114,7 @@ function Navbar({ isLoggedIn, setIsLoggedIn }) {
   };
 
   return (
-    <div className="floating-navbar">
+    <div className={`floating-navbar ${menuOpen ? "active" : ""}`}>
       <span className="nav-logo" onClick={() => navigate("/")}>
         Genescope
       </span>
@@ -35,17 +126,15 @@ function Navbar({ isLoggedIn, setIsLoggedIn }) {
         <span></span>
         <span></span>
       </div>
-
+{/* 
       <div
         className={`nav-overlay ${menuOpen ? "active" : ""}`}
         onClick={toggleMenu}
-      ></div>
+      ></div> */}
 
       <div className={`nav-links ${menuOpen ? "active" : ""}`}>
         <button
-          className={`nav-button ${
-            location.pathname === "/" ? "active-tab" : ""
-          }`}
+          className={`nav-button ${location.pathname === "/" ? "active-tab" : ""}`}
           onClick={() => navigate("/")}
         >
           Home
@@ -54,24 +143,18 @@ function Navbar({ isLoggedIn, setIsLoggedIn }) {
         {isLoggedIn && (
           <>
             <button
-              className={`nav-button ${
-                location.pathname === "/fileupload" ? "active-tab" : ""
-              }`}
+              className={`nav-button ${location.pathname === "/fileupload" ? "active-tab" : ""}`}
               onClick={() => navigate("/fileupload")}
             >
               File Upload
             </button>
 
             <button
-              className={`nav-button ${
-                location.pathname === "/myfiles" ? "active-tab" : ""
-              }`}
+              className={`nav-button ${location.pathname === "/myfiles" ? "active-tab" : ""}`}
               onClick={() => navigate("/myfiles")}
             >
               My Files
             </button>
-
-            
           </>
         )}
 
@@ -89,8 +172,3 @@ function Navbar({ isLoggedIn, setIsLoggedIn }) {
 }
 
 export default Navbar;
-
-
-
-
-
