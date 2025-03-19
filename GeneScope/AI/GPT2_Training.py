702a1432d2b975afe54fc920403cf591165a3d7f
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2025-03-18 22:38:53 -0400

    updating Ai code

--- GeneScope/AI/GPT2_Training.py
+++ GeneScope/AI/GPT2_Training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03c95a720dc7d467dfa88853e16a2e6106a3d2542640feefee70eba5221a04da
-size 1664
+oid sha256:2ab6b7b0e7e1c28e4489bfe5efc9baf7d4467139ed19303c9996f5959b07691b
+size 1719
--- GeneScope/AI/generator.py
+++ GeneScope/AI/generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:509f2c5dd42df19a3d415e7a4f05f008cbee0d14d4bbfc7bcd5f34cb088298cb
-size 3166
+oid sha256:55da6687deada5fb2b85be619626e0b0746d197167854eed446a06cc55088b5b
+size 3519
