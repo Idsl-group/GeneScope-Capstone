3bc487bff03571667a76e3532f54760b522e4728
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2025-03-18 23:10:41 -0400

    Ai fine tuned

--- GeneScope/AI/GPT2_Training.py
+++ GeneScope/AI/GPT2_Training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ab6b7b0e7e1c28e4489bfe5efc9baf7d4467139ed19303c9996f5959b07691b
-size 1719
+oid sha256:f60bacb23418fedc3a5865e412430726b47a076b50c9be7a6efdb4eed61224b2
+size 1730
